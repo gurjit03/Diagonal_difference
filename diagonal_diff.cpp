f128f44cbb3531208c21477292ec4d305dbd3af0
gurjit03 <gurjitmehta@gmail.com>
2015-07-21 23:20:37 +0530

    created diagonal_diff.cpp file

---
+++ diagonal_diff.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+int n;
+cin>>n;
+int n1 = n;
+int arr[n][n];
+
+int main_diagonal = 0;
+
+int sec_diagonal = 0;
+
+for( int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++) {
+                cin>>arr[i][j];
+        }
+}
+
+
+int i1 = 0,j1 = 0;
+
+for(int i = 0; i < n; i++) {
+ main_diagonal += arr[i1][j1];
+ //cout<<main_diagonal<<"---- main diagonal "<<i1<<j1<<endl;
+ i1++;
+ j1++;
+}
+
+for( int i = 0; i < n; i++) {
+ n1--;	
+ sec_diagonal += arr[i][n1];
+ //cout<<sec_diagonal<<"---- sec diagonal "<<i<<n1<<endl;
+}
+
+int diff = main_diagonal - sec_diagonal;
+ 
+diff = diff < 0 ? diff * -1 : diff;
+cout<<diff;
+return 0;
+}       
+
